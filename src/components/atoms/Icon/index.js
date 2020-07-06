3467d75fa8a00e0f6cb76a359b45aac3f14348ba
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-07-07 01:17:36 +0900

    Add Img, Heading, and Icon components

---
+++ src/components/atoms/Icon/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './styles.css';
+
+export const iconFactory = (iconName) => (props) => (
+  <IconContainer
+    presenter={(presenterProps) => <IconPresenter {...presenterProps} />}
+    {...{ iconName, ...props }}
+  />
+);
+
+export const IconPresenter = ({
+  iconName,
+  height = 20,
+  width = 20,
+  ...props
+}) => (
+  <img
+    src={`/icons/${iconName}.svg`}
+    alt=""
+    height={height}
+    width={width}
+    {...props}
+  />
+);
+
+export const IconContainer = ({
+  presenter,
+  className = '',
+  onClick,
+  ...props
+}) => {
+  if (onClick) {
+    className += `${styles.clickable}`;
+  }
+  return presenter({ onClick, className, ...props });
+};
+
+export const TrashCanIcon = iconFactory('trash-can');
+export const ChevronRightIcon = iconFactory('chevron-right');
+export const SearchIcon = iconFactory('search');
+export const SettingsIcon = iconFactory('settings');
